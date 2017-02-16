9e026576f2e8f5127bd9d71df96ca5c8a442ceca
Jakob Sunde <jsunde@uw.edu>
2017-02-16 01:43:24 -0800

    More processing of paper text, finished assigning papers to their authors

--- Main.py
+++ Main.py
@@ -4,36 +4,72 @@
 # petergg
 # 2/15/2017
 
-papers = {'HAMILTON': [], 'JAY': [], 'MADISON': [], 'UNKNOWN': []}
-authors = papers.keys()
+# Stores the content of the papers
+papers = []
+
+# Stores a list of the paper numbers that correspond to each author
+authorsToPaperNumbers = {'HAMILTON': [], 'JAY': [], 'MADISON': [], 'DISPUTED': []}
+
+# Stores the names of the authors, including 'DISPUTED'
+authors = authorsToPaperNumbers.keys()
+
+# Set disputed papers
+for i in range(49, 59):
+	authorsToPaperNumbers['DISPUTED'].append(i)
+
+authorsToPaperNumbers['DISPUTED'].append(62)
+authorsToPaperNumbers['DISPUTED'].append(63)
+
+# Set joint papers
+for i in range(18, 21):
+	authorsToPaperNumbers['HAMILTON'].append(i)
+	authorsToPaperNumbers['MADISON'].append(i)
 
 def readData(fileName):
 	f = open(fileName, 'r')
 
-	lines = f.readlines()
+	lines = [line.lstrip().rstrip() for line in f.readlines()]
 	start = -1
-	currAuthor=''
+	currAuthor = ''
+	currPaperNum = 0
+
 	for i in range(len(lines)):
-		line = lines[i].lstrip().rstrip()
+		line = lines[i]
 		
 		# If we're not currently in the middle of a paper
 		# check for the start of a paper
-		# TODO: add logic to keep track of which federalist paper this is
+		if line.startswith('FEDERALIST'):
+			currPaperNum = int(line.split(' ')[-1])
+
 		if start == -1:
 			for author in authors:
 				if line.startswith(author):
-					currAuthor = author
-					start = i
+					currAuthor = author					
+
+					# Set start to be four lines later, so as not to include author name
+					# or paper header ('To the people of ...') in analysis of paper contents
+					start = i + 4
 					break
 
-		if line == 'PUBLIUS':
-			papers[currAuthor].append(lines[start:i])
+		# If at the end of a paper, add contents of paper to papers
+		# and corresponding paper number to list for current author
+		if line.startswith('PUBLIUS'):
+			# If not disputed or joint,
+			# store the current paper number that corresponds to the current author
+			for author in authors:
+				if currPaperNum not in authorsToPaperNumbers[author]:
+					authorsToPaperNumbers[currAuthor].append(currPaperNum)
+			
+			# Add content of current paper to papers
+			papers.append(lines[start:i])
 
 			# Set start to -1 to indicate we are not in the middle of a paper
 			start = -1
 
 def main():
 	readData('papers.txt')
+
+	print authorsToPaperNumbers['HAMILTON']
 	
 if __name__ == '__main__':
     main()
